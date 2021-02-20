ef3072a9c4226b00d582da20ecef98bae6579f9d
unknown <nmartinezb6@miumg.edu.gt>
2021-02-19 21:54:46 -0600

    estoy iniciando proyecto

---
+++ pruebaGib/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pruebaGib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

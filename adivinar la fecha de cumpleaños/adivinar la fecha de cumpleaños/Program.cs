eb2fc0bcdbb023cb234dbb808a853e2a96e476f8
nmartinezb6 <79180002+nmartinezb6@users.noreply.github.com>
2021-03-05 17:22:25 -0600

    Add files via upload

---
+++ adivinar la fecha de cumpleaños/adivinar la fecha de cumpleaños/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace adivinar_la_fecha_de_cumpleaños
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int calculo, dato;
+            string dia, mes, resultado;
+
+            Console.WriteLine("Piensa en el dia que naciste");
+            Console.ReadKey();
+            Console.WriteLine("Ahora multiplicalo por 20");
+            Console.ReadKey();
+            Console.WriteLine("Al resultado sumale 73");
+            Console.ReadKey();
+            Console.WriteLine("Ahora multiplicalo por 5");
+            Console.ReadKey();
+            Console.WriteLine("Ahora sumale la fecha en que nacisiste, es decir el numero del mes");
+            Console.ReadKey();
+            Console.WriteLine("Ppr favor insgresa el resultado de tu operacion");
+            dato = int.Parse(Console.ReadLine());
+            calculo = dato - 365;
+
+            resultado = Convert.ToString(calculo);
+
+            dia = resultado.Substring(0, 2);
+            mes = resultado.Substring(2, 2);
+
+
+
+
+            Console.WriteLine("El dia en que naciste es:" + dia);
+            Console.WriteLine("El mes en que naciste es:" + mes);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ adivino el numero que piensas/adivino el numero que piensas/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace adivino_el_numero_que_piensas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor, divicion, resta;
+
+            Console.WriteLine("piense en un numero, no lo olvide");
+            Console.ReadKey();
+
+
+            Console.WriteLine(" ahora multiplique su numero por 2 ");
+            Console.ReadKey();
+
+            Console.WriteLine(" al numero resultante sumele 8 ");
+            Console.ReadKey();
+
+            Console.WriteLine(" al resultado multipliquelo por 5 ");
+            Console.ReadKey();
+
+            Console.WriteLine("ingrese el resultado");
+            valor = Convert.ToInt32(Console.ReadLine());
+
+            divicion = valor / 10;
+            resta = divicion - 4;
+
+
+
+            Console.WriteLine("el numero resultante es: {0}", resta);
+            Console.ReadKey();
+
+        }
+    }
+}
